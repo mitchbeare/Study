b6f63c0d9d24d0397b6cb68038047a41295e3970
Mitchell Beare <Mitchellbeare@gmail.com>
2020-08-27 10:29:32 +1000

    Built some if's

--- AutomateTheBoringStuffWithPython/Excercises/ifElse.py
+++ AutomateTheBoringStuffWithPython/Excercises/ifElse.py
@@ -1,5 +1,16 @@
-name = 'Alice'
-if name == 'Alice'
-    print('Hi Alice')
-print('Done')
+password = 'Apples'
+if password == 'Apples':
+    print('Access Granted')
+else:
+    print('Access Denied')
 
+name = 'Bob'
+age = 3000
+if name == 'Alice':
+    print('He Alice')
+elif age < 12:
+    print('You are not Alice, kiddo')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead immortal vampire')
+elif age > 100:
+    print('You are not Alice, Grannie')
