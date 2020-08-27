efaa78a7f7acf94138331440dc8e2f4f0d85924c
Mitchell Beare <Mitchellbeare@gmail.com>
2020-08-27 10:46:20 +1000

    Added while loop example

---
+++ AutomateTheBoringStuffWithPython/Excercises/while.py
@@ -0,0 +1,11 @@
+spam = 0
+while spam < 3:
+    print('Hello World!')
+    spam += 1
+
+
+name = ''
+while name != 'Billy':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
